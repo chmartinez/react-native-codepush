3d9e56df8f24eb1dac6fc6d807a1899d818b43f8
christian <c.martinez@mytaxi.com>
2018-11-13 10:12:49 +0100

    refactor: Add codepush sync on mounting phase

--- ReactNativeCodePush/App.js
+++ ReactNativeCodePush/App.js
@@ -8,7 +8,7 @@
 
 import React, { Component } from 'react';
 import codePush from 'react-native-code-push';
-import { Button, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Button, Platform, StyleSheet, Text, View } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,20 +18,15 @@ const instructions = Platform.select({
 });
 
 type Props = {};
-/**
- * EL PASO 6.1 NO FUNCIONO. LA APP NUNCA ME PIDIO HACER UN UPDATE FORZADO
- * https://codeburst.io/react-native-updates-with-vs-app-center-codepush-3d56ef07f1c4
- * VER ESTO!
- */
 export default class App extends Component<Props> {
-  onPress = () => alert('CodePush is in da house!');
-
-  onCodePushTrigger = () => {
+  componentDidMount() {
     codePush.sync({
       updateDialog: true,
       installMode: codePush.InstallMode.IMMEDIATE,
     });
   }
+
+  onPress = () => alert('Feature not ready');
 
   render() {
     return (
@@ -39,9 +34,6 @@ export default class App extends Component<Props> {
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Button title="Check this out!" onPress={this.onPress} />
-        <TouchableOpacity onPress={this.onCodePushTrigger}>
-          <Text>Check for updates</Text>
-        </TouchableOpacity>
       </View>
     );
   }
